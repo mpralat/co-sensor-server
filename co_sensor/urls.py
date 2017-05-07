69b6a7e09796d5eeffdba57069c41c6e8f73e6e2
Nuurek <nurskijakub@gmail.com>
2017-05-07 22:14:16 +0200

    Prepared home page.

--- co_sensor/urls.py
+++ co_sensor/urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
+    url(r'^$',
+        TemplateView.as_view(template_name="co_sensor/home.html"),
+        name='home'),
     url(r'^accounts/', include('accounts.urls')),
     url(r'^admin/', admin.site.urls),
 ]
