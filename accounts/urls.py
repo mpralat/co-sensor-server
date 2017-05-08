a2f3d111ed4f787abb8525e42b8fd9a022b12aed
Nuurek <nurskijakub@gmail.com>
2017-05-08 13:48:55 +0200

    Refactored signup views.

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,21 +1,29 @@
 from django.conf.urls import url
 from django.contrib.auth import views as auth_views
 from django.views.generic import TemplateView
-from .views import signup, activate
+from .views import SignUpView, AccountActivateView
 
 urlpatterns = [
-    url(r'^signup/', signup, name='signup'),
+    url(r'^signup/', SignUpView.as_view(), name='signup'),
+    url(r'^account_activation_sent/$',
+        TemplateView.as_view(
+            template_name="accounts/account_activation_sent.html"
+        ),
+        name='account_activation_sent'),
+    url(r'^activate/' +
+        r'(?P<uidb64>[0-9A-Za-z_\-]+)/' +
+        r'(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        AccountActivateView.as_view(),
+        name='activate'),
+    url(r'^account_activation_invalid',
+        TemplateView.as_view(
+            template_name="accounts/account_activation_invalid.html"
+        ),
+        name='account_activation_invalid'),
     url(r'^login/',
         auth_views.login, {
             'template_name': 'accounts/login.html',
         },
         name='login'),
     url(r'^logout/', auth_views.logout, name='logout'),
-    url(r'^account_activation_sent/$',
-        TemplateView.as_view(
-            template_name="accounts/account_activation_sent.html"
-        ),
-        name='account_activation_sent'),
-    url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-        activate, name='activate'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,50 +1,85 @@
 from django.contrib.auth import login
 from django.contrib.auth.models import User
 from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
 from django.shortcuts import render, redirect
 from django.utils.encoding import force_bytes, force_text
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
-from django.template.loader import render_to_string
+from django.views.generic import TemplateView, View
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.core.mail import send_mail
 from .forms import SignUpForm
 from .tokens import account_activation_token
 
 
-def signup(request):
-    if request.method == 'POST':
-        form = SignUpForm(request.POST)
-        if form.is_valid():
-            user = form.save(commit=False)
-            user.is_active = False
-            user.save()
-            current_site = get_current_site(request)
-            subject = 'Activate Your MySite Account'
-            message = render_to_string(
-                'accounts/account_activation_email.html', {
-                    'user': user,
-                    'domain': current_site.domain,
-                    'uid': urlsafe_base64_encode(force_bytes(user.pk)),
-                    'token': account_activation_token.make_token(user),
-                }
-            )
-            user.email_user(subject, message)
-            return redirect('account_activation_sent')
-    else:
-        form = SignUpForm()
-    return render(request, 'accounts/signup.html', {'form': form})
+class SignUpView(TemplateView):
+    template_name = 'accounts/signup.html'
+
+    def get(self, request, *args, **kwargs):
+        self.form = SignUpForm()
+        return super().get(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        self.form = SignUpForm(request.POST)
+        if self.form.is_valid():
+            user = self.__save_user()
+
+            self.__send_activation_mail(user, request)
+
+            return HttpResponseRedirect(reverse('account_activation_sent'))
+        else:
+            return super().get(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = self.form
+        return context
+
+    def __save_user(self):
+        user = self.form.save(commit=False)
+        user.is_active = False
+        user.save()
+        return user
+
+    def __send_activation_mail(self, user, request):
+        domain = get_current_site(request).domain
+        subject = 'Activate Your CO Sensor Account'
+        message = render_to_string(
+            'accounts/account_activation_email.html', {
+                'user': user,
+                'domain': domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': account_activation_token.make_token(user),
+            }
+        )
+        send_mail(
+            subject=subject,
+            message='Activate Your CO Sensor Account',
+            from_email="no-reply@" + domain,
+            recipient_list=[user.email],
+            html_message=message
+        )
 
 
-def activate(request, uidb64, token):
-    try:
-        uid = force_text(urlsafe_base64_decode(uidb64))
-        user = User.objects.get(pk=uid)
-    except (TypeError, ValueError, OverflowError, User.DoesNotExist):
-        user = None
+class AccountActivateView(View):
 
-    if user is not None and account_activation_token.check_token(user, token):
+    def get(self, request, uidb64, token, *args, **kwargs):
+        try:
+            uid = force_text(urlsafe_base64_decode(uidb64))
+            user = User.objects.get(pk=uid)
+        except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+            user = None
+
+        is_token_valid = account_activation_token.check_token(user, token)
+        if user is not None and is_token_valid:
+            self.__activate_user(user)
+            login(request, user)
+            return HttpResponseRedirect(reverse('home'))
+        else:
+            return HttpResponseRedirect(reverse('account_activation_invalid'))
+
+    def __activate_user(self, user):
         user.is_active = True
         user.profile.email_confirmed = True
         user.save()
-        login(request, user)
-        return redirect('home')
-    else:
-        return render(request, 'account_activation_invalid.html')
