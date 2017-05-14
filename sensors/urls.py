fe6c18701e3039a1b01799c594ab270f4aafec2e
Nuurek <nurskijakub@gmail.com>
2017-05-14 11:53:33 +0200

    Fixed room url.

--- sensors/urls.py
+++ sensors/urls.py
@@ -4,5 +4,5 @@ from .views import chat_room, SensorListView
 
 urlpatterns = [
     url(r'^list', SensorListView.as_view(), name='sensors_list'),
-    url(r'^room/(?P<label>[0-0A-F]{16})', chat_room, name='room'),
+    url(r'^room/(?P<label>[0-9A-F]{16})', chat_room, name='room'),
 ]
