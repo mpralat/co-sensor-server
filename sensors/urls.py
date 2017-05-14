a61b43e1e9852097b4105fe020a20785d0e24d6b
Nuurek <nurskijakub@gmail.com>
2017-05-14 11:51:23 +0200

    Added widget-tweaks app to requirements.txt. Added sensor url to sensors list.

--- sensors/urls.py
+++ sensors/urls.py
@@ -3,6 +3,6 @@ from .views import chat_room, SensorListView
 
 
 urlpatterns = [
-    url(r'^list', SensorListView.as_view(), name="sensors_list"),
-    url(r'^room/(?P<label>[0-9A-Za-z_\-]+)', chat_room, name='room'),
+    url(r'^list', SensorListView.as_view(), name='sensors_list'),
+    url(r'^room/(?P<label>[0-0A-F]{16})', chat_room, name='room'),
 ]
